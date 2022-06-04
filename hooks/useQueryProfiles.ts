d3285fb3f693547c4fdc139cfcb5e77ef7b24c23
kiyo7 <kiyo324783@icloud.com>
2022-06-04 22:35:53 +0900

    customHook実装

---
+++ hooks/useQueryProfiles.ts
@@ -0,0 +1,53 @@
+import { useQuery } from 'react-query'
+import { supabase } from '../utils/supabase'
+import useStore from '../store'
+import { Profile } from '../types'
+import { useMutateProfile } from './useMutateProfile'
+
+export const useQueryProfiles = () => {
+  const session = useStore((state) => state.session)
+  const editedProfile = useStore((state) => state.editedProfile)
+  const updateEditedProfile = useStore((state) => state.updateEditedProfile)
+
+  const { createProfileMutation } = useMutateProfile()
+
+  const getProfile = async () => {
+    const { data, error, status } = await supabase
+      .from('profiles')
+      .select('*')
+      .eq('id', session?.user?.id)
+      .single()
+
+    if (error && status === 406) {
+      createProfileMutation.mutate({
+        id: session?.user?.id,
+        username: session?.user?.email,
+        favorites: '',
+        avatar_url: '',
+      })
+      updateEditedProfile({
+        ...editedProfile,
+        username: session?.user?.email,
+      })
+    }
+    if (error && status !== 406) {
+      throw new Error(error.message)
+    }
+    return data
+  }
+
+  return useQuery<Profile, Error>({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+    staleTime: Infinity,
+    onSuccess: (data) => {
+      if (data) {
+        updateEditedProfile({
+          username: data.username,
+          favorites: data.favorites,
+          avatar_url: data.avatar_url,
+        })
+      }
+    },
+  })
+}
